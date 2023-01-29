01b263140af7d7c1ab7b8589b951871cbc5cab75
Dso Tsin <dsotsen@gmail.com>
2023-01-29 19:19:03 +0800

    update lfs

--- src/libgit2/lfs.c
+++ src/libgit2/lfs.c
@@ -2,87 +2,115 @@
 
 #include "filter.h"
 #include "str.h"
+#include "transports/auth_lfs.h"
+#include "git2/blob.h"
+
+#define MAX_JSON 4096
 
 static int lfs_stream(
-        git_writestream **out,
-        git_filter *self,
-        void **payload,
-        const git_filter_source *src,
-        git_writestream *next);
-static void lfs_begin_sync(git_filter *self);
-static void lfs_end_sync(git_filter *self);
-static int lfs_init(git_filter *self);
-static void lfs_shutdown(git_filter *self);
+    git_writestream** out,
+    git_filter* self,
+    void** payload,
+    const git_filter_source* src,
+    git_writestream* next);
+static void lfs_begin_sync(git_filter* self);
+static void lfs_end_sync(git_filter* self);
+static int lfs_init(git_filter* self);
+static void lfs_shutdown(git_filter* self);
 static int lfs_prefilter(
-        git_filter *self,
-        void **payload,
-        const git_filter_source *src,
-        const git_blob *blob,
-        const char **attr_values);
+    git_filter* self,
+    void** payload,
+    const git_filter_source* src,
+    const git_blob* blob,
+    const char** attr_values);
 
-git_filter *git_lfs_fetch_filter_new(void)
+git_filter* git_lfs_fetch_filter_new(void)
 {
-	lfs_filter *f = git__calloc(1, sizeof(lfs_filter));
-	if (f == NULL)
-		return NULL;
+    lfs_filter* f = git__calloc(1, sizeof(lfs_filter));
+    if (f == NULL)
+	return NULL;
 
-	f->parent.version	 = GIT_FILTER_VERSION;
-	f->parent.attributes = "filter=lfs";
-	f->parent.initialize = lfs_init;
-	f->parent.shutdown   = lfs_shutdown;
-	f->parent.stream     = lfs_stream;
-	f->parent.pre_filter = lfs_prefilter;
-	f->parent.begin_sync = lfs_begin_sync;
-	f->parent.end_sync   = lfs_end_sync;
+    f->parent.version = GIT_FILTER_VERSION;
+    f->parent.attributes = "filter=lfs";
+    f->parent.initialize = lfs_init;
+    f->parent.shutdown = lfs_shutdown;
+    f->parent.stream = lfs_stream;
+    f->parent.pre_filter = lfs_prefilter;
+    f->parent.begin_sync = lfs_begin_sync;
+    f->parent.end_sync = lfs_end_sync;
 
-	f->pool = init_workerpool(8);
+    f->pool = init_workerpool(8);
 
-	return f;
+    return f;
 }
 
 int lfs_stream(
-        git_writestream **out,
-        git_filter *self,
-        void **payload,
-        const git_filter_source *src,
-        git_writestream *next)
+    git_writestream** out,
+    git_filter* self,
+    void** payload,
+    const git_filter_source* src,
+    git_writestream* next)
 {
-	lfs_filter *lfs = self;
-	return 0;
+    lfs_filter* lfs = self;
+    return 0;
 }
 
 int lfs_prefilter(
-        git_filter *self,
-        void **payload, /* NULL on entry, may be set */
-        const git_filter_source *src,
-        const git_blob *blob,
-        const char **attr_values)
+    git_filter* self,
+    void** payload, /* NULL on entry, may be set */
+    const git_filter_source* src,
+    const git_blob* blob,
+    const char** attr_values)
 {
-	lfs_filter *lfs = self;
-	return 0;
+    lfs_filter* lfs = self;
+    git_repository* repo = git_filter_source_repo(src);
+    git_remote* remote = NULL;
+    int error = git_remote_lookup(&remote, repo, "origin");
+    if (error != 0)
+    {
+	return error;
+    }
+    git_reference* head = NULL;
+    error = git_repository_head(&head, repo);
+    const char* ref_name = git_reference_name(head);
+    const char* url = git_remote_url(remote);
+    char json[MAX_JSON] = {0};
+    error = git_lfs_authenticate(url, NULL/*TODO*/, git_lfs_command_download, json, MAX_JSON);
+
+    if (lfs->auth == NULL)
+    {
+	lfs->auth = git__calloc(1, sizeof(lfs_auth));
+    }
+
+    const char* content = (const char*)git_blob_rawcontent(blob);
+    git_object_size_t size = git_blob_rawsize(blob);
+
+    return error;
 }
 
-void lfs_begin_sync(git_filter *self)
+void lfs_begin_sync(git_filter* self)
 {
-	lfs_filter *lfs = self;
+    lfs_filter* lfs = self;
 }
 
-void lfs_end_sync(git_filter *self)
+void lfs_end_sync(git_filter* self)
 {
-	lfs_filter *lfs = self;
+    lfs_filter* lfs = self;
 }
 
-int lfs_init(git_filter *self)
+int lfs_init(git_filter* self)
 {
-	lfs_filter *lfs = self;
+    lfs_filter* lfs = self;
 
-	return 0;
+    return 0;
 }
 
-void lfs_shutdown(git_filter *filter)
+void lfs_shutdown(git_filter* filter)
 {
-	lfs_filter *lfs = filter;
-	wait_workerpool(lfs->pool);
-	close_workerpool(lfs->pool);
-	lfs->pool = NULL;
+    lfs_filter* lfs = filter;
+    wait_workerpool(lfs->pool);
+    close_workerpool(lfs->pool);
+    lfs->pool = NULL;
 }
+
+#undef MAX_JSON
--- src/libgit2/lfs.h
+++ src/libgit2/lfs.h
@@ -5,9 +5,16 @@
 #include "git2/sys/filter.h"
 #include "workerpool.h"
 
+typedef struct lfs_auth {
+    git_str href;
+    git_str hdr;
+    unsigned int expires;
+} lfs_auth;
+
 typedef struct lfs_filter {
-	git_filter parent;
-	workerpool *pool;
+    git_filter parent;
+    workerpool* pool;
+    lfs_auth* auth;
 } lfs_filter;
 
 #endif
